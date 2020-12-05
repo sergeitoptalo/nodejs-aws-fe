70cd58347b96f37e1d2eac7c2b54c621d90208da
siarhei_taptala <Siarhei_Taptala@epam.com>
2020-12-05 13:05:52 +0300

    feat: add cart api endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://t1dpmo9bgg.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://o1oewsft1f.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://http://sergeitoptalo-cart-api-dev.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
