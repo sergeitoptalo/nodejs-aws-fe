b866a69dd2fe9b2d0a596e08ef960f976d1424b4
siarhei_taptala <Siarhei_Taptala@epam.com>
2020-12-12 13:44:03 +0300

    feat: add bff paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,8 +3,8 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://t1dpmo9bgg.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://o1oewsft1f.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://sergeitoptalo-cart-api-dev.eu-west-1.elasticbeanstalk.com',
+  bff: 'https://jl32md4xz7.execute-api.eu-west-1.amazonaws.com/dev/product-service',
+  cart: 'https://jl32md4xz7.execute-api.eu-west-1.amazonaws.com/dev/cart-service',
 };
 
 export default API_PATHS;
